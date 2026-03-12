2ac065294868c5097c3990936936b7503d0a9694
rihrds <151781904+rihrds@users.noreply.github.com>
2026-03-12 14:49:19 +0200

    Uzlaboju kodu
    Pierakstiju labakus komentarus
    Iesaku koda noteiksanas funkciju. Vel jauztaisa MI dala, kas nosaka ciparus

--- extraction/process_image.py
+++ extraction/process_image.py
@@ -7,15 +7,8 @@ import re
 
 """
 Add error checking - ie missing number, etc
-Add proper debug checks in get_title and get_answers_box
 Create function for extracting the student's code
 """
-
-
-
-
-
-
 
 def extract_document(image, debug=False):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -30,6 +23,7 @@ def extract_document(image, debug=False):
         cv2.imshow('normalized illumination', norm)
         cv2.waitKey(0)
 
+    #detect edges
     blurred = cv2.GaussianBlur(norm, (11, 11), 0)
     edges = cv2.Canny(blurred, 30, 90)
     if debug:
@@ -42,6 +36,7 @@ def extract_document(image, debug=False):
         cv2.imshow('edges after dilation', edges)
         cv2.waitKey(0)
 
+    #find contours
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
     result = image.copy()
@@ -109,9 +104,6 @@ def get_topic_boxes(image, debug=False):
 
 
 
-###
-#Can be improved
-###
 def get_box_title(image, box, debug=False):
     x, y, w, h = cv2.boundingRect(box)
 
@@ -152,7 +144,7 @@ def get_box_answers(image, box, debug=False):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    #group circles per question
+    #group circles per row
     rows = {}
     for cx, cy, r in np.round(circles[0]).astype(int):
         matched = False
@@ -164,7 +156,7 @@ def get_box_answers(image, box, debug=False):
         if not matched:
             rows[cy] = [(cx, cy, r)]
 
-    #determine how many columns of questions are there
+    #split rows per questions
     questions = []
     for row_y, row_circles in sorted(rows.items()):
         x_positions = sorted(set(c[0] for c in row_circles))
@@ -196,6 +188,7 @@ def get_box_answers(image, box, debug=False):
         #clean question_num
         question_num = re.sub(r'[^0-9]', '', question_num)
 
+        #determine which circle is filled
         means = []
         for cx, cy, r in sorted_question:
             mask = np.zeros(gray.shape, dtype=np.uint8)
@@ -205,11 +198,65 @@ def get_box_answers(image, box, debug=False):
         if debug:
             print(question_num + ":", means)
         if min(means) > 100:
-            answers[question_num] = "ABCD"[means.index(min(means))]
+            answers[question_num] = "ABCDEF"[means.index(min(means))]
 
     sorted_answers = dict(sorted(answers.items(), key=lambda x: int(x[0])))
 
     return sorted_answers
+
+
+def get_student_code(image, debug=False):
+    h, _ = image.shape[:2]
+
+    roi = image[int(0.025*h):int(0.2*h), :]
+    if debug:
+        copy = roi.copy()
+
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    edges = cv2.Canny(blurred, 30, 90)
+    if debug:
+        cv2.imshow('edges before dilation', edges)
+        cv2.waitKey(0)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    edges = cv2.dilate(edges, kernel)
+    if debug:
+        cv2.imshow('edges after dilation', edges)
+        cv2.waitKey(0)
+
+
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    roi_area = roi.shape[0] * roi.shape[1]
+
+    boxes = []
+    for cnt in contours:
+        peri = cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+        area = cv2.contourArea(approx)
+
+        if len(approx) == 4 and 0.005 * roi_area < area < roi_area * 0.2:
+            boxes.append(approx)
+            if debug:
+                cv2.drawContours(copy, [cnt], -1, (0, 255, 0), 4)
+
+    if debug:
+        cv2.imshow('code boxes', copy)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    #sort boxes based on x coordinates
+    boxes = sorted(boxes, key=lambda b: cv2.boundingRect(b)[0])
+
+    box_images = []
+    for box in boxes:
+        x, y, w, h = cv2.boundingRect(box)
+        box_images.append(roi[y:y+h, x:x+w])
+
+    ########
+    #pass these images to AI to determine what number is written
+    ########
 
 
 def get_answers(image, debug=False):
@@ -224,11 +271,13 @@ def get_answers(image, debug=False):
 
         result[title] = answers
 
+    get_student_code(document_img, debug=True)
+
     if debug:
         print(result)
 
     return result
 
 img = cv2.imread('image.jpg')
-get_answers(img, True)
+get_answers(img, False)
 
